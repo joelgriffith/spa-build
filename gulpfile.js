c799ea50b6763483938c8cfb8f7eac6e604e31a5
Joel Griffith <jgriffith@appnexus.com>
2014-07-31 11:05:28 -0700

    Imp

--- gulpfile.js
+++ gulpfile.js
@@ -1,176 +1,100 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr(),
-    gutil = require('gulp-util'),
-    info = require('./package.json'),
-	webpack = require('webpack'),
-
-	// Sources and outputs
-	base = './src',
-	dev = './build/dev',
-	dist = './build/dist',
-	js = base + '/js',
-	scss = base + '/scss',
-	assets = base + '/assets',
-	jsIndex = js + '/index.js',
-	scssIndex = scss + '/index.scss',
-
-	// Contstants
-	EXPRESS_PORT = 8080,
-	EXPRESS_ROOT = dev,
-	LIVERELOAD_PORT = 35729,
-	IMG_COMPRESSION = 3,
-	SASS_SOURCE_MAPS = false,
-	SASS_COMPASS = false,
-
-	// Comment Generation
-	title = info.name,
-	author = info.author,
-	version = info.version,
-	build = Date.now(),
-
-	// JS Package
-	webpackConfigBase = {
-		cache: true,
-		entry: jsIndex,
-		output: {
-			filename: "index.js"
-		},
-		resolve: {
-			modulesDirectories: ['node_modules', 'bower_components']
-		},
-		plugins: [
-			new webpack.BannerPlugin(title + '\n' + author + '\n' + version + ':' + build)
-		]
-	};
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+var livereload = require('gulp-livereload');
+var webpack = require('gulp-webpack');
+var lr = require('tiny-lr');
+var server = lr();
+var gutil = require('gulp-util');
+var gulpConfig = require('./gulp.config');
 
 // Tasks
-gulp.task('default', ['clean'], function() {
-    gulp.start('hint', 'webpack:dev', 'webpack:dist', 'styles:dev', 'styles:dist', 'images:dev', 'images:dist', 'html:dev', 'html:dist');
-});
-gulp.task('build:dev', ['clean'], function() {
-    gulp.start('hint', 'webpack:dev', 'styles:dev', 'images:dev', 'html:dev');
-});
-gulp.task('build:dist', ['clean'], function() {
-    gulp.start('hint', 'webpack:dist', 'styles:dist', 'images:dist', 'html:dist');
-});
+gulp.task('default', ['hint', 'webpack', 'styles', 'images', 'html']);
 
 // JS packaging for distribution
-gulp.task('webpack:dist', function() {
-	var myConfig = Object.create(webpackConfigBase);
-	myConfig.output.path = dist + '/js';
-	myConfig.plugins = myConfig.plugins.concat(
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		}),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin()
-	);
-	webpack(myConfig, function(err, stats) {
-		if(err) throw new gutil.PluginError('webpack:dist', err);
-		gutil.log('[webpack:dist]', stats.toString({
-			colors: true
-		}));
-	});
-});
-gulp.task('webpack:dev', function() {
-	var myConfig = Object.create(webpackConfigBase);
-	myConfig.output.path = dev + '/js';
-	myConfig.plugins = myConfig.plugins.concat(
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		})
-	);
-	webpack(myConfig, function(err, stats) {
-		if(err) throw new gutil.PluginError('webpack:dev', err);
-		gutil.log('[webpack:dev]', stats.toString({
-			colors: true
-		}));
-		return gulp.src(dev).pipe(livereload(server));
-	});
+gulp.task('webpack', ['clean'], function() {
+	return gulp.src(gulpConfig.scripts.paths.entry)
+		.pipe(changed(gulpConfig.scripts.paths.output.dev))
+		.pipe(webpack(gulpConfig.webpack))
+		.pipe(gulp.dest(gulpConfig.scripts.paths.output.dev))
+		.pipe(uglify())
+		.pipe(gulp.dest(gulpConfig.scripts.paths.output.prod))
+		;
 });
 gulp.task('hint', function() {
-  return gulp.src(js + '/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'));
+	return gulp.src(gulpConfig.scripts.paths.all)
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		;
 });
-gulp.task('styles:dist', function() {
-  return gulp.src(scssIndex)
-    .pipe(sass({ style: 'compressed' }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(minifycss())
-    .pipe(gulp.dest(dist + '/css'));
+gulp.task('styles', ['clean'], function() {
+	return gulp.src(gulpConfig.styles.paths.entry)
+		.pipe(sass())
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest(gulpConfig.styles.paths.output.dev))
+		.pipe(sass())
+		.pipe(minifycss())
+		.pipe(gulp.dest(gulpConfig.styles.paths.output.prod))
+		;
 });
-gulp.task('styles:dev', function() {
-  return gulp.src(scssIndex)
-    .pipe(sass({ style: 'expanded', sourcemap: SASS_SOURCE_MAPS, compass: SASS_COMPASS }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest(dev + '/css'))
-    .pipe(livereload(server));
+gulp.task('images', ['clean'], function() {
+	return gulp.src(gulpConfig.images.paths.all)
+		.pipe(gulp.dest(gulpConfig.images.paths.output.dev))
+		.pipe(imagemin({
+			optimizationLevel: gulpConfig.images.compression,
+			progressive: true,
+			interlaced: true
+		}))
+		.pipe(gulp.dest(gulpConfig.images.paths.output.prod))
+		;
 });
-gulp.task('images:dist', function() {
-  return gulp.src(assets + '/**/*')
-    .pipe(imagemin({ optimizationLevel: IMG_COMPRESSION, progressive: true, interlaced: true }))
-    .pipe(gulp.dest(dist + '/assets'));
-});
-gulp.task('images:dev', function() {
-  return gulp.src(assets + '/**/*')
-    .pipe(gulp.dest(dev + '/assets'))
-    .pipe(livereload(server));
-});
-gulp.task('html:dist', function() {
-	return gulp.src(base + '/*.html')
-		.pipe(gulp.dest(dist));
-});
-gulp.task('html:dev', function() {
-	return gulp.src(base + '/*.html')
-		.pipe(gulp.dest(dev))
-		.pipe(livereload(server));
+gulp.task('html', ['clean'], function() {
+	return gulp.src(gulpConfig.html.paths.entry)
+		.pipe(gulp.dest(gulpConfig.html.paths.output.dev))
+		.pipe(gulp.dest(gulpConfig.html.paths.output.prod))
+		;
 });
 gulp.task('clean', function() {
-  return gulp.src([dist, dev], {read: false})
-    .pipe(clean());
+	return gulp.src(['./build/dev', './build/prod'], {
+			read: false
+		})
+		.pipe(clean());
 });
-gulp.task('connect', function() {
+gulp.task('connect', ['clean'], function() {
 	var express = require('express');
 	var app = express();
 	app.use(require('connect-livereload')());
-	app.use(express['static'](EXPRESS_ROOT));
-	app.listen(EXPRESS_PORT);
+	app.use(express['static'](gulpConfig.express.root));
+	app.listen(gulpConfig.express.root);
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['clean'], function() {
 	gulp.start('connect');
-	server.listen(LIVERELOAD_PORT, function (err) {
+	server.listen(gulpConfig.liveReload.port, function(err) {
 		if (err) {
 			return console.log(err);
 		}
-		gulp.watch(base + '/scss/*.scss', function(event) {
+		gulp.watch(gulpConfig.styles.paths.all, function(event) {
 			gutil.log('Watch:', 'File ' + event.path + ' was ' + event.type + ', running tasks...');
-			gulp.start('styles:dev');
+			gulp.start('styles');
 		});
-		gulp.watch(base + '/js/*.js', function(event) {
+		gulp.watch(gulpConfig.scripts.paths.all, function(event) {
 			gutil.log('Watch:', 'File ' + event.path + ' was ' + event.type + ', running tasks...');
-			gulp.start('hint', 'webpack:dev');
+			gulp.start('hint', 'webpack');
 		});
-		gulp.watch(base + '/assets/img/**/*', function(event) {
+		gulp.watch(gulpConfig.images.paths.all, function(event) {
 			gutil.log('Watch:', 'File ' + event.path + ' was ' + event.type + ', running tasks...');
-			gulp.start('images:dev');
+			gulp.start('images');
 		});
-		gulp.watch(base + '/index.html', function(event) {
+		gulp.watch(gulpConfig.html.paths.entry, function(event) {
 			gutil.log('Watch:', 'File ' + event.path + ' was ' + event.type + ', running tasks...');
-			gulp.start('html:dev');
+			gulp.start('html');
 		});
 	});
 });
